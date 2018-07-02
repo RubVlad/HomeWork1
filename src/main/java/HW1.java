ee472e6287e70797c8b5425ccbe16f3c7fae6992
Vladislav Rublevskiy <vladislavr@123software.ru>
2018-07-02 14:01:33 +0300

    Initial commit

---
+++ src/main/java/HW1.java
@@ -0,0 +1,14 @@
+public class HW1 {
+    public static void main(String[] args){
+    int a,b,c=0;
+    float answ=0;
+    a=2;
+    b=3;
+
+    if(a%2==0)
+    answ = a * b / c;
+    if ((a+c)%2==0||(b+c)%2==0)
+    answ = a * b / c;
+        System.out.println(answ);
+    }
+}

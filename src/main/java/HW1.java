6de64afaf3b50826d1576da77a4965eb00f2bd60
RubVlad <40755490+RubVlad@users.noreply.github.com>
2018-07-03 15:23:46 +0300

    Update HW1.java

--- src/main/java/HW1.java
+++ src/main/java/HW1.java
@@ -1,14 +1,17 @@
 public class HW1 {
-    public static void main(String[] args){
-    int a,b,c=0;
-    float answ=0;
-    a=2;
-    b=3;
+        public static void main(String[] args){
+            int a,b,c=0;
+            float answ=0;
+            a=2;
+            b=3;
+            c=1;
 
-    if(a%2==0)
-    answ = a * b / c;
-    if ((a+c)%2==0||(b+c)%2==0)
-    answ = a * b / c;
-        System.out.println(answ);
-    }
+            if(a%2==0)
+                answ = a * b / c;
+            if ((a+c)%2==0||(b+c)%2==0)
+                answ = a * b / c;
+            else
+                answ = a+b-c;
+            System.out.println(answ);
+        }
 }

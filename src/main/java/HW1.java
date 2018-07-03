9a72db9bb46760501e9cd21d43c132d9733f47cb
RubVlad <40755490+RubVlad@users.noreply.github.com>
2018-07-03 16:41:51 +0300

    Update HW1.java

--- src/main/java/HW1.java
+++ src/main/java/HW1.java
@@ -2,7 +2,9 @@ public class HW1 {
         public static void main(String[] args){
             calc();
             factorial();
+            reverse();
         }
+
 
 
         public static void calc()
@@ -24,6 +26,7 @@ public class HW1 {
 
         }
 
+
         public static void factorial()
         {
             int n = 3;
@@ -33,4 +36,45 @@ public class HW1 {
 
             System.out.println(fact);
         }
+
+
+    public static void reverse()
+    {
+        int x=554636;
+
+        int tempx=0;
+
+        int ost=1;
+        int des=0;
+        int desatie=0;
+
+        int answ=0;
+
+
+        while (x!=0)
+        {
+            ost=x%10;
+            tempx=x;
+            for (int i=1; tempx!=0; i++)
+            {
+                tempx=tempx/10;
+                des=i;
+            }
+
+            x=x/10;
+
+            desatie = 1;
+
+            for(int i=0; i<des-1;i++)
+            {
+                desatie = desatie*10;
+            }
+
+                answ = answ + (ost*desatie);
+        }
+
+        System.out.println(answ);
+
+    }
 }
+

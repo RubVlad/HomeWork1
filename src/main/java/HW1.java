40be2491394f2fee26c2d7316b155a3126ffc497
RubVlad <40755490+RubVlad@users.noreply.github.com>
2018-07-03 18:40:43 +0300

    Update HW1.java

--- src/main/java/HW1.java
+++ src/main/java/HW1.java
@@ -1,77 +1,80 @@
+import java.util.Scanner;
+//import static java.lang.Math.*;
+
+
 public class HW1 {
-        public static void main(String[] args){
-            calc();
-            factorial();
-            reverse();
-            bubble();
-        }
+
+    static Scanner in = new Scanner(System.in);
 
 
-
-        public static void calc()
-        {
-            int a,b,c=0;
-            float answ=0;
-            a=2;
-            b=3;
-            c=1;
-
-            if(a%2==0)
-                answ = a * b / c;
-            if ((a+c)%2==0||(b+c)%2==0)
-                answ = a * b / c;
-            else
-                answ = a+b-c;
-
-            System.out.println(answ);
-
-        }
+    public static void main(String[] args) {
+        calc();
+        //factorial();
+       // reverse();
+       // bubble();
+    }
 
 
-        public static void factorial()
-        {
-            int n = 3;
-            int fact=1;
-                for (int i=1; i<n; i++)
-                fact = fact*i;
+    public static void calc() {
+        int a, b, c = 0;
+        float answ = 0;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
 
-            System.out.println(fact);
-        }
+        if (a % 2 == 0)
+            answ = a * b / c;
+        if ((a + c) % 2 == 0 || (b + c) % 2 == 0)
+            for (int i = 0; i < c; i++) {
+                a = a * a;
+            }
+        else
+            answ = a + b - c;
 
+        System.out.println(answ);
 
-    public static void reverse()
-    {
-        int x=554636;
-
-        int tempx=0;
-
-        int ost=1;
-        int des=0;
-        int desatie=0;
-
-        int answ=0;
+    }
 
 
-        while (x!=0)
-        {
-            ost=x%10;
-            tempx=x;
-            for (int i=1; tempx!=0; i++)
-            {
-                tempx=tempx/10;
-                des=i;
+    public static void factorial() {
+        int n = 3;
+        int fact = 1;
+        for (int i = 1; i < n; i++)
+            fact = fact * i;
+
+        System.out.println(fact);
+    }
+
+
+    public static void reverse() {
+        int x = 554636;
+
+        int tempx = 0;
+
+        int ost = 1;
+        int des = 0;
+        int desatie = 0;
+
+        int answ = 0;
+
+        while (x != 0) {
+            ost = x % 10;
+            tempx = x;
+            for (int i = 1; tempx != 0; i++) {
+                tempx = tempx / 10;
+                des = i;
             }
 
-            x=x/10;
+            x = x / 10;
 
             desatie = 1;
 
-            for(int i=0; i<des-1;i++)
-            {
-                desatie = desatie*10;
+
+            for (int i = 0; i < des - 1; i++) {
+                desatie = desatie * 10;
             }
 
-                answ = answ + (ost*desatie);
+            answ = answ + (ost * desatie);
         }
 
         System.out.println(answ);
@@ -83,7 +86,7 @@ public class HW1 {
         int tmp = 0;
         int[] mas = {5, 3, 2, 4};
 
-        for (int i = mas.length-1; i > 0; i--) {
+        for (int i = mas.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
 
                 if (mas[j] > mas[j + 1]) {
@@ -97,7 +100,6 @@ public class HW1 {
         for (int i = 0; i < mas.length; i++)
             System.out.println(mas[i]);
     }
-
 
 }
 
